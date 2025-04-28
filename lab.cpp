e2330dd6c602995be4881c56754e24a651b387c2
Gary9864 <00021925@uca.edu.sv>
2025-04-28 08:10:11 -0600

    Add files via upload

---
+++ lab.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int edad = 0;
+    cout << "ingrese la edad de la persona:  ";
+    cin >> edad;
+
+    if(edad < 5){
+        cout << " El niño puede entrar gratis";
+    
+    }else if(edad < 12){
+        cout << "Debe de pagar 5 dolares por la entrada";
+    }else{
+        cout << "Debe de pagar 10 dolares por la entrada";
+    }
+    cout << "\nMuchas gracias por su visita";
+    return 0;
+
+}
